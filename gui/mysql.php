0ffd3c4d149421bd84849e738db3c5c4983b7713
Skizzo3000 <git@skizzo3000.it>
2011-04-04 00:10:06 +0200

    Modifiche su php

---
+++ gui/mysql.php
@@ -0,0 +1,31 @@
+<?php
+
+/**********************
+** Funzioni database **
+**********************/
+
+/** 
+- Connetti DB 
+- Crea tabella slave_tab
+- Test tabella slave_tab
+- Cerca slave by addr
+- Cerca slave by name
+- Cerca slave by error
+- Cerca slave by enable
+- Aggiungi slave
+- Rimuovi  slave
+- Crea tabella slave_name-addr
+- Rimuovi tabella slave_name-addr
+- Cerca registro/coil by address
+- Cerca registro/coil by name
+- Cerca registro/coil by type
+- Aggiungi registro/coil
+- Rimuovi registro/coil
+
+Ogni funzione di inserimento deve includere una prericerca per evitare duplicati che cmq non sono permessi dal DB
+Ogni funzione di rimozione deve includere il controllo di esistenza
+
+**/
+
+
+?>
